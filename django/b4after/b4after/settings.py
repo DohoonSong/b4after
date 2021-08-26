3042856b9cf711393b5714a33e74a8b27136687c
DohoonSong <ztehgns@gmail.com>
2021-08-26 13:59:21 +0900

    mainPage html add

--- django/b4after/b4after/settings.py
+++ django/b4after/b4after/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os, json
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'mainpage.apps.MainpageConfig',
+
 ]
 
 MIDDLEWARE = [
@@ -54,7 +56,9 @@ ROOT_URLCONF = 'b4after.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, "mainpage", "template")
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,6 +122,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    
+    BASE_DIR / "static",
+    ("mainpage", os.path.join(BASE_DIR, "mainpage", "static")),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- django/b4after/mainpage/views.py
+++ django/b4after/mainpage/views.py
@@ -1,7 +1,24 @@
 from django.shortcuts import render, HttpResponse, get_object_or_404
-from .models import Coffee
+from django.template import loader
+# from .models import Coffee
 
 import json 
 # Create your views here.
-def index2(request):
+def index(request):
+    args = {
+        "name" : "arg"
+    }
     return render(request, "index.html", args)
+
+def index2(request):
+    return HttpResponse("Hello, world. You're at the polls now.")
+
+def index3(request):
+    template = loader.get_template("index.html")
+    args = {
+        "a": 'b'
+    }
+    return HttpResponse(template.render(args, request))
+
+def generic(request):
+	return render(request, "generic.html")
